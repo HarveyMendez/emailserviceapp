1db518ff9f6c40b741c89fe59410d07956db5788
HarveyMendez <harvey.mendez@ucr.ac.cr>
2024-06-12 13:33:41 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/send-email', (req, res) => {
+    const { email, subject, message } = req.body;
+
+    // Configura tu transportador de correo usando variables de entorno
+    const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: "harvey.mendez02@gmail.com",
+            pass: "sfef rrin qelc mhld",
+        }
+    });
+
+    const mailOptions = {
+        from: "harvey.mendez02@gmail.com",
+        to: email,
+        subject: subject,
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Correo enviado: ' + info.response);
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
